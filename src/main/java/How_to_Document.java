dc2dca9601204513caca30395859ac1eb69adb58
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-11-24 19:48:54 +0100

    Documentation Guide

---
+++ src/main/java/How_to_Document.java
@@ -0,0 +1,23 @@
+/**
+ * Please make a full description of your Method and use the Plugin JavaDoc to generate your Documentation
+ * <p>
+ * Javadoc can be found in IntelliJ via pressing (Strg + Alt + S) to Access your Setting´s and by chosing Plugins
+ * you can search for it.
+ * <p>
+ * Javadoc Hotkey to generate Documentation is Shift + Strg + Alt + G
+ * <p>
+ * please dont forget to add @author : Your Name
+ * Example below:
+ * <p>
+ * example @param url
+ * an absolute URL giving the base location of the image
+ * example @param name
+ * the location of the image, relative to the url argument
+ *
+ * @author Andreas Scheuer
+ * @return the image at the specified URL
+ * example @see Image
+ */
+
+public class How_to_Document {
+}
