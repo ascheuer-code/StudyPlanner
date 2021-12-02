5c2aa04f9363e5755150f1126a7eda8716b627f2
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-12-02 17:46:50 +0100

    Helper Class Test

--- src/test/java/Model/HelperTest.java
+++ src/test/java/Model/HelperTest.java
@@ -1,5 +1,8 @@
 package Model;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -9,4 +12,98 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class HelperTest {
 
+    /**
+     * Check span.
+     *
+     * @author Andreas Scheuer
+     */
+    @Test
+    void checkSpan() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Helper.checkSpan(0, 1, 2, "Test");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Helper.checkSpan(0, 1, -1, "Test");
+        });
+    }
+
+    /**
+     * Check min.
+     *
+     * @author Andreas Scheuer
+     */
+    @Test
+    void checkMin() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Helper.checkMin(0, -1, "Test");
+        });
+
+    }
+
+    /**
+     * Check max.
+     *
+     * @author Andreas Scheuer
+     */
+    @Test
+    void checkMax() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Helper.checkMax(1, 2, "Test");
+        });
+    }
+
+    /**
+     * Number formatter.
+     *
+     * @author Andreas Scheuer
+     */
+    @Test
+    void numberFormatter() {
+    }
+
+    /**
+     * Check if null or empty.
+     *
+     * @author Andreas Scheuer
+     */
+    @Test
+    void checkIfNullOrEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Helper.checkIfNullOrEmpty("", "Test");
+        });
+
+        String testString = null;
+        assertThrows(NullPointerException.class, () -> {
+            Helper.checkIfNullOrEmpty(testString, "Test");
+        });
+    }
+
+    /**
+     * All whitespace to one.
+     *
+     * @author Andreas Scheuer
+     */
+    @Test
+    void allWhitespaceToOne() {
+        assertEquals("Affe hat drei Haare", Helper.allWhitespaceToOne("Affe     hat     drei     Haare"));
+    }
+
+    /**
+     * Remove all whitespaces.
+     *
+     * @author Andreas Scheuer
+     */
+    @Test
+    void removeAllWhitespaces() {
+        assertEquals("AffehatdreiHaare", Helper.removeAllWhitespaces("Affe     hat     drei     Haare"));
+    }
+
+    /**
+     * Trim array list.
+     *
+     * @author Andreas Scheuer
+     */
+    @Test
+    void trimArrayList() {
+    }
 }
