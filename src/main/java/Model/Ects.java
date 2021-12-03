1817c7679bc4bab38c9e65e96ee2d8f171def2fd
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-12-03 18:06:17 +0100

    Ects refactoring ideas for Leon

--- src/main/java/Model/Ects.java
+++ src/main/java/Model/Ects.java
@@ -6,19 +6,22 @@ package Model;
  * @author leon weyand
  */
 public class Ects {
+    private static final String ECTS_POSITIVE = "Ects need to be positive!"; // <- this is not needed because the Error Message will be generated in Message Class with the Helper Method
     private int ects;
     private int minutes;
-    private Helper helper;
-    private static final String ECTS_POSITIVE = "Ects need to be positive!";
+    private Helper helper; // not needed see next comment below
 
     /**
      * Instantiates a new Ects.
      *
-     * @param ects the ECTS that need to be converted to minutes.
+     * @param ects
+     *         the ECTS that need to be converted to minutes.
      */
     public Ects(int ects) {
-        helper = new Helper();
-        helper.checkMin(1, ects, ECTS_POSITIVE);
+        // @Leon The Helper class is Static so no Object will be needed just use Helper.checkMin(min,Value,Fieldname) thats all :D
+        helper = new Helper(); // <- unused
+        //helper.checkMin(1, ects, ECTS_POSITIVE);
+        Helper.checkMin(1, ects, "ECTS"); // <- like this
         this.ects = ects;
         this.minutes = ectsToMinutes(ects);
     }
@@ -26,11 +29,14 @@ public class Ects {
     /**
      * Calculates the amount of minutes to learn for the ECTS.
      *
-     * @param ects the ECTS that need to be converted to minutes.
+     * @param ects
+     *         the ECTS that need to be converted to minutes.
+     *
      * @return the minutes to learn.
      */
     private int ectsToMinutes(int ects) {
-        return ects*60*30;
+        // please put in the Comments how they will be calculated with a Source
+        return ects * 60 * 30;
     }
 
     /**
