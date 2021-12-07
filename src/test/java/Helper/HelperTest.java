eefb6342261264161fc1c22f4530fe9a1c2604ba
Andreas Scheuer <69506130+ascheuer-code@users.noreply.github.com>
2021-12-07 14:23:51 +0100

    Helper moved

--- src/test/java/Helper/HelperTest.java
+++ src/test/java/Helper/HelperTest.java
@@ -1,9 +1,9 @@
-package Model;
+package Helper;
 
-import org.apache.commons.lang3.ObjectUtils;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * The type Helper test.
