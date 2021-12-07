321fdb7fc6b70712ba0f34a41156dd5d05a9f46e
Andreas Scheuer <69506130+ascheuer-code@users.noreply.github.com>
2021-12-07 13:02:32 +0100

    added comments

--- src/main/java/Model/Event.java
+++ src/main/java/Model/Event.java
@@ -4,13 +4,45 @@ package Model;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+/**
+ * The type Event.
+ */
 public class Event {
+    /**
+     * The Modulname.
+     */
     public String modulname;
+    /**
+     * The Anfagszeit.
+     */
     public LocalTime anfagszeit;
+    /**
+     * The Endzeit.
+     */
     public  LocalTime endzeit;
+    /**
+     * The Datum.
+     */
     public LocalDate datum;
+    /**
+     * The Beschreibung.
+     */
     public  String beschreibung;
 
+    /**
+     * Instantiates a new Event.
+     *
+     * @param modulname
+     *         the modulname
+     * @param anfangszeit
+     *         the anfangszeit
+     * @param endzeit
+     *         the endzeit
+     * @param datum
+     *         the datum
+     * @param beschreibung
+     *         the beschreibung
+     */
     public Event( String modulname,LocalTime anfangszeit,LocalTime endzeit,LocalDate datum, String beschreibung){
         this.modulname= modulname;
         this.anfagszeit= anfangszeit;
@@ -18,6 +50,11 @@ public class Event {
         this.datum= datum;
         this.beschreibung= beschreibung;}
 
+    /**
+     * Get modulname string.
+     *
+     * @return the string
+     */
     public String getModulname(){
         return modulname;
     }
@@ -27,6 +64,11 @@ public class Event {
         return  modulname +"  "+ anfagszeit + "  "+ endzeit+ "  " +datum + "  "+beschreibung;
     }
 
+    /**
+     * To string 2 string.
+     *
+     * @return the string
+     */
     public String toString2() {
         return  modulname;
     }
