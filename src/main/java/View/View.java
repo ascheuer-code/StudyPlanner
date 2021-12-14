18e6af3e7980e1fe5b8b4be463fca0e258594bb2
mjmay <mjmayer2001@gamil.com>
2021-12-14 14:59:33 +0100

    Frontend wurde Kommentiert und beim Button neuer Eintrag wurde der Platzhalter gegen eine ChocieBox getauscht welche alle Module zur auswahl stellt

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,8 +1,28 @@
 package View;
 
+import Model.Event;
+import Model.Modul;
+import com.calendarfx.model.Calendar;
+import com.calendarfx.model.Entry;
+import impl.com.calendarfx.view.NumericTextField;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The type View.
  * @author Andreas Scheuer
  */
 public class View {
+
 }
