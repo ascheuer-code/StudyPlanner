993f59079cc3f68696394476f6ee46a5be243ae9
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-12-20 14:57:25 +0100

    Event class prepared for DB

--- src/main/java/Model/Event.java
+++ src/main/java/Model/Event.java
@@ -1,42 +1,93 @@
 package Model;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+
 
 public class Event {
-    public String modulname;
-    public LocalTime anfagszeit;
-    public LocalTime endzeit;
-    public LocalDate datum;
-    public String beschreibung;
+    private String id;
+    private String title;
+    private String starDate;
+    private String startTime;
+    private String endDate;
+    private String endTime;
 
-    public Event(String modulname, LocalTime anfangszeit, LocalTime endzeit, LocalDate datum, String beschreibung) {
-        this.modulname = modulname;
-        this.anfagszeit = anfangszeit;
-        this.endzeit = endzeit;
-        this.datum = datum;
-        this.beschreibung = beschreibung;
+    public Event(String id, String title, String starDate, String startTime, String endDate, String endTime) {
+        this.id = id;
+        this.title = title;
+        this.starDate = starDate;
+        this.startTime = startTime;
+        this.endDate = endDate;
+        this.endTime = endTime;
     }
 
-    /**
-     * @return String
-     */
-    public String getModulname() {
-        return modulname;
+    public Event(){
+        
+        
     }
 
-    /**
-     * @return String
-     */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStarDate() {
+        return starDate;
+    }
+
+    public void setStarDate(String starDate) {
+        this.starDate = starDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
     @Override
     public String toString() {
-        return modulname + "  " + anfagszeit + "  " + endzeit + "  " + datum + "  " + beschreibung;
+        return "Event{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", starDate='" + starDate + '\'' +
+                ", startTime='" + startTime + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", endTime='" + endTime + '\'' +
+                '}';
     }
 
     /**
      * @return String
      */
     public String toString2() {
-        return modulname;
+        return title;
     }
 }
