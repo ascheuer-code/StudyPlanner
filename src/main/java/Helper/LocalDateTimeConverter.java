e12fc48a3f23cbb50537b42b2c8ce586df8466bb
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-12-20 14:57:55 +0100

    Converter added

---
+++ src/main/java/Helper/LocalDateTimeConverter.java
@@ -0,0 +1,72 @@
+package Helper;
+
+import Model.Event;
+import com.calendarfx.model.Entry;
+import com.calendarfx.model.Interval;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * The type Local date time converter.
+ */
+public class LocalDateTimeConverter {
+
+
+    /**
+     * Convert Entry to Event.
+     *
+     * @param entry the entry
+     * @return the event
+     *
+     * @author Andreas Scheuer
+     */
+    public static Event convertEntrytoEvent(Entry entry) {
+
+        Event event = new Event();
+
+        event.setId(entry.getId());
+        event.setTitle(entry.getTitle());
+        event.setStarDate(entry.getInterval().getStartDate().toString());
+        event.setStartTime(entry.getInterval().getStartTime().toString());
+        event.setEndDate((entry.getInterval().getEndDate().toString()));
+        event.setEndTime((entry.getInterval().getEndTime().toString()));
+
+        return event;
+   }
+
+    /**
+     * Convert Event to Entry.
+     *
+     * @param event the event
+     * @return the entry
+     *
+     * @author Andreas Scheuer
+     */
+    public static Entry convertEventToEntry(Event event){
+
+        Entry entry = new Entry();
+
+        entry.setId(event.getId());
+        entry.setTitle(event.getTitle());
+        entry.setInterval(convertEventIntervalToEntryInterval(event));
+
+        return entry;
+   }
+
+    /**
+     * Convert Date and Time string to Interval .
+     *
+     * @param event the event
+     * @return the interval
+     *
+     * @autor Andreas Scheuer
+     */
+    public static Interval convertEventIntervalToEntryInterval(Event event) {
+
+        Interval interval =  new Interval(LocalDate.parse(event.getStarDate()), LocalTime.parse(event.getStartTime()), LocalDate.parse(event.getEndDate()), LocalTime.parse(event.getEndTime()));
+
+        return interval;
+    }
+
+}
