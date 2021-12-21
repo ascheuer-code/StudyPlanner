d3604ba740461d5657ea447c57d85105da7ab241
Irrenhauspatient <andreasscheuer85@gmx.de>
2021-12-21 13:05:08 +0100

    universally unique identifier succesfully tested / disabled after the first run

--- src/test/java/Model/EntryTest.java
+++ src/test/java/Model/EntryTest.java
@@ -1,6 +1,8 @@
 package Model;
 
 import com.calendarfx.model.Entry;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EntryTest {
 
-    @Test
+    @RepeatedTest(value = 10, name= "testidUnique {currentRepetition}/{totalRepetitions}")
+    @Disabled //Achtung dauert ne Zeit lang !!!!
     void testIdUnique() {
         ArrayList<Entry> test = new ArrayList<Entry>();
         ArrayList<Integer> intArray = new ArrayList<>();
