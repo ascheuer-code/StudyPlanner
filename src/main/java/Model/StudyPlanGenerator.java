09b963370242c005525e3aa1a80c79f0820155bf
Irrenhauspatient <andreasscheuer85@gmx.de>
2022-04-09 11:17:04 +0200

    final 1.0

--- src/main/java/Model/StudyPlanGenerator.java
+++ src/main/java/Model/StudyPlanGenerator.java
@@ -23,8 +23,10 @@ public class StudyPlanGenerator {
     List<Modul> modules;
     Calendar studyPlan;
     HashMap<Modul, Integer> moduleDaysHashMap;
+    HashMap<Modul, Integer> moduleDaysHashMap2;
     EntityManager entityManager;
     EntityTransaction entityTransaction;
+    int counter = -1;
 
     /**
      * Initiates a new StudyPlanGenerator
@@ -36,13 +38,14 @@ public class StudyPlanGenerator {
      * @param entityTransaction
      */
     public StudyPlanGenerator(List<Event> events, List<Modul> modules, com.calendarfx.model.Calendar studyPlan,
-            EntityManager entityManager, EntityTransaction entityTransaction) {
+                              EntityManager entityManager, EntityTransaction entityTransaction) {
         this.events = events;
         this.modules = modules;
         this.studyPlan = studyPlan;
         this.entityManager = entityManager;
         this.entityTransaction = entityTransaction;
         moduleDaysHashMap = new HashMap<>();
+        moduleDaysHashMap2 = new HashMap<>();
     }
 
     /**
@@ -52,7 +55,6 @@ public class StudyPlanGenerator {
     public void start() {
         modules.sort(Comparator.comparing(modul -> modul.gettEcts()));
         Collections.reverse(modules);
-        modules.stream().forEach(e -> System.out.println(e));
         calculateModuleDays();
         generateStudyPlan();
     }
@@ -107,7 +109,30 @@ public class StudyPlanGenerator {
     private List<Event> getFillerEvents() {
         List<Event> fillerEvents = new ArrayList<>();
         events.stream().filter(e -> e.getTitle().trim().equals("Filler")).forEach(e -> fillerEvents.add(e));
+        sortFillerEvents(fillerEvents);
         return fillerEvents;
+    }
+
+    /**
+     * Sorts Date and Time of event List.
+     *
+     * @param listToSort the fillerEvents that need to be sorted
+     * @return a sorted List with fillerEvents
+     * @author Leon
+     */
+    private List<Event> sortFillerEvents(List<Event> listToSort) {
+        for(int i = 0; i < listToSort.size()-1; i++) {
+            for(int j = 0; j < listToSort.size() - 1; j++) {
+                String strI = listToSort.get(j+1).getStarDate() + ";" + listToSort.get(j+1).getStartTime();
+                String strJ = listToSort.get(j).getStarDate() + ";" + listToSort.get(j).getStartTime();
+                if(strI.compareTo(strJ) < 0) {
+                    Event temp = listToSort.get(j);
+                    listToSort.set(j, listToSort.get(j+1));
+                    listToSort.set(j+1, temp);
+                }
+            }
+        }
+        return listToSort;
     }
 
     /**
@@ -131,12 +156,30 @@ public class StudyPlanGenerator {
      * @author Leon
      */
     private void generateStudyPlan() {
-
-        for (Event event : getFillerEvents()) {
+        List<Event> fillerEvents = getFillerEvents();
+        int maxDays = fillerEvents.size();
+        if(maxDays > 10) {
+            fillHashMap2(maxDays, 10);
+        } else {
+            fillHashMap2(maxDays, maxDays);
+        }
+        for (Event event : fillerEvents) {
             checkModuleDurations();
 
             if (moduleDaysHashMap.size() == 0) {
                 break;
+            }
+
+            if(counter < 9) {
+                counter++;
+            } else {
+                counter = 0;
+                maxDays -= 10;
+                if(maxDays >= 10) {
+                    fillHashMap2(maxDays, 10);
+                } else {
+                    moduleDaysHashMap2 = moduleDaysHashMap;
+                }
             }
 
             Modul ranModule = getRandomModule();
@@ -182,16 +225,45 @@ public class StudyPlanGenerator {
      */
     private Modul getRandomModule() {
         Random random = new Random();
-        int index = random.nextInt(moduleDaysHashMap.size());
-        List<Modul> tempModules = new ArrayList<>(moduleDaysHashMap.keySet());
-        tempModules.stream().forEach(e -> System.out.println(e));
+        int index = random.nextInt(moduleDaysHashMap2.size());
+        List<Modul> tempModules = new ArrayList<>(moduleDaysHashMap2.keySet());
         Modul module = tempModules.get(index);
-        moduleDaysHashMap.replace(module, moduleDaysHashMap.get(module) - 1);
+        moduleDaysHashMap2.replace(module, moduleDaysHashMap2.get(module) - 1);
 
-        if (moduleDaysHashMap.get(module) == 0) {
-            moduleDaysHashMap.remove(module);
+        if (moduleDaysHashMap2.get(module) == 0) {
+            moduleDaysHashMap2.remove(module);
         }
         return module;
+    }
+
+    /**
+     * Fills the HashMap2 with modules and days.
+     * HashMap2 is a smaller part of HashMap.
+     * Deletes the Module from the HashMap, when the value is 0.
+     *
+     * @param maxDays the number of fillerEvents.
+     * @param fillDays is a smaller number than maxDays.
+     * @author Leon
+     */
+    private void fillHashMap2(int maxDays, int fillDays) {
+        List<Modul> tempModules = new ArrayList<>(moduleDaysHashMap.keySet());
+        int currDays = 0;
+        for (Modul m : tempModules) {
+            double days = (moduleDaysHashMap.get(m) / (double) maxDays) * fillDays;
+            moduleDaysHashMap2.put(m, (int) days);
+            currDays += (int) days;
+        }
+
+        for (Modul m : tempModules) {
+            if(10 - currDays > 0) {
+                moduleDaysHashMap2.replace(m, moduleDaysHashMap2.get(m) + 1);
+                currDays++;
+            }
+            moduleDaysHashMap.replace(m, moduleDaysHashMap.get(m) - moduleDaysHashMap2.get(m));
+            if(moduleDaysHashMap.get(m) == 0) {
+                moduleDaysHashMap.remove(m);
+            }
+        }
     }
 
     /**
